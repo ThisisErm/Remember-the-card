8e7787f92df75e21f520b33ffb72696a0e69cb94
Erm <ermithon@gmail.com>
2023-01-04 15:47:26 -0500

    completed correct/incorrect user story

--- index.js
+++ index.js
@@ -1,18 +1,44 @@
-   // hide the grid by default
-//    document.querySelector('.game-grid').style.display = 'none';
+// variables
+const cards = document.querySelectorAll('.card-content');
+const gameGrid = document.querySelector('.game-grid');
+const messageDiv = document.querySelector('#results');
 
-// //    // show the grid when the button is clicked
-//    document.querySelector('.play-button').addEventListener('click', () => {
-//      document.querySelector('.game-grid').style.display = 'block';
-//    });
-   
-// get all the cards
-const cards = document.querySelectorAll('.grid-item');
+
+//Store card clicks
+let card1
+let card2
+
+
+//Check if cards clicked match
+function checkMatch() {
+  if (card1 === card2) {
+    messageDiv.innerHTML = 'Match :)';
+  } else {
+    messageDiv.innerHTML = 'No Match :(';
+  }
+}
 
 // add a click event listener to each card
 cards.forEach(card => {
   card.addEventListener('click', () => {
-    // show the image when the card is clicked
+// show the image when the card is clicked
     card.querySelector('img').style.display = 'block';
-  });
+
+
+//grab the ID of first & second card clicked
+if (!card1) {
+  card1 = card.getAttribute('id')
+} else {
+  card2 = card.getAttribute('id')
+}
+
+//check if the ids are the same
+if (card1 && card2) {
+  checkMatch();
+card1 = null;
+card2 = null;
+}
+     
+  });  
 });
+
