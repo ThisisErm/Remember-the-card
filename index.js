bcb5be0e5628437bdbfcefb0eb36f7f1c7b41c8b
Erm <ermithon@gmail.com>
2023-01-05 14:44:34 -0500

    Added check if win + restart game user stories

--- index.js
+++ index.js
@@ -1,21 +1,64 @@
-// variables
 const cards = document.querySelectorAll('.card-content');
 const gameGrid = document.querySelector('.game-grid');
-const messageDiv = document.querySelector('#results');
+let results = document.querySelector('#results');
+let goodGuess = 0 
+let badGuess = 0
+ 
+// restart game
+const playButton = document.querySelector('.play-button')
 
+playButton.addEventListener('click', () => {
+  gameGrid.style.display = 'grid';
+  card1 = null;
+  card2 = null;
+  goodGuess = 0
+  badGuess = 0
+  results.innerHTML = "Choose your cards"
+   cards.forEach(card => {
+    card.querySelector('img').style.display = 'none';
+  });
+});
 
 //Store card clicks
 let card1
 let card2
 
-
-//Check if cards clicked match
+//Check for card matches
 function checkMatch() {
-  if (card1 === card2) {
-    messageDiv.innerHTML = 'Match :)';
+    if (card1 === card2) {
+    results.innerHTML = 'Match :)';
   } else {
-    messageDiv.innerHTML = 'No Match :(';
+    results.innerHTML = 'No Match :(';
+    cards.forEach(card => {
+      if (card.getAttribute('id') === card1 || card.getAttribute('id') === card2) {
+        card.querySelector('img').style.display = 'none';
+      }
+    })
+    }
+}
+//Record good & bad guesses into variables 
+  checkResults = function (){
+    if (results.innerHTML == 'Match :)') {
+    goodGuess++
+    console.log(`good guess counter: ${goodGuess}`)
+  } else {
+    badGuess++
+    console.log(`bad guess counter: ${badGuess}`)
   }
+}
+
+//Win the game
+function winGame(){
+  if (goodGuess > 7) {
+  results.innerHTML = 'Congratulations. You win!'
+} 
+} 
+
+//lose the game 
+function loseGame(){
+  if (badGuess > 4) {
+    results.innerHTML = 'You lose. Try again.'
+} 
 }
 
 // add a click event listener to each card
@@ -24,7 +67,6 @@ cards.forEach(card => {
 // show the image when the card is clicked
     card.querySelector('img').style.display = 'block';
 
-
 //grab the ID of first & second card clicked
 if (!card1) {
   card1 = card.getAttribute('id')
@@ -32,13 +74,18 @@ if (!card1) {
   card2 = card.getAttribute('id')
 }
 
-//check if the ids are the same
+//Run functions after 2 clicks
 if (card1 && card2) {
-  checkMatch();
+  checkMatch()
+  checkResults()
+  winGame()
+  loseGame()
+  
+  
+//reset the clicks for next turn  
 card1 = null;
 card2 = null;
 }
      
   });  
 });
-
