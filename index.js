c8af91b4e792faa8c55e1200ff9efb27932ce3fd
Erm <ermithon@gmail.com>
2023-01-10 13:13:42 -0500

    renamed readme

--- index.js
+++ index.js
@@ -10,15 +10,13 @@ let card1
 let card2 
 //button
 const playButton = document.querySelector('.play-button')
-const winButton = document.querySelector('.win-button')
-const loseButton = document.querySelector('.lose-button')
-
 
 // restart game
 playButton.addEventListener('click', () => {
   gameGrid.style.display = 'grid'
   card1 = null
   card2 = null
+  Arr1 = []
   goodGuess = 0
   badGuess = 0
   attempts = 5
@@ -27,11 +25,27 @@ playButton.addEventListener('click', () => {
   card.querySelector('img').style.display = 'none'
   card.classList.remove('clicked')
   // card.classList.remove('matched')
-  Arr1 = []
 
   })
 })
 
+//Check for card matches
+function checkMatch() {
+  if (card1 == card2) {
+  results.innerHTML = 'Match :)'
+} else {
+  results.innerHTML = `No Match. You have ${attempts} attempts left`
+}
+}
+
+//Record good & bad guesses into variables 
+grabResults = function(){
+  if (results.innerHTML == 'Match :)') {
+  goodGuess++
+} else {
+  badGuess++
+}
+}
 
 //Add 'matched' class if match
 function addMatch() {
@@ -44,23 +58,6 @@ function addMatch() {
   }  
 }
 
-//Add 'unmatched' class if cards don't match
-function noMatch(){
-  if(card1 != card2){
-    document.getElementById(card1).querySelector('img').classList.add('noMatch')
-    document.getElementById(card2).querySelector('img').classList.add('noMatch')
-    
-}}
-
-//Check for card matches
-function checkMatch() {
-  if (card1 == card2) {
-  results.innerHTML = 'Match :)'
-} else {
-  results.innerHTML = `No Match. You have ${attempts} attempts left`
-  noMatch()
-}
-}
 
 // // //Hide cards if they don't match
 function hideUnmatched() {
@@ -73,23 +70,11 @@ function hideUnmatched() {
 }
   
 
-     
-//Record good & bad guesses into variables 
-  grabResults = function(){
-    if (results.innerHTML == 'Match :)') {
-    goodGuess++
-    console.log(`good guess counter: ${goodGuess}`)
-  } else {
-    badGuess++
-    console.log(`bad guess counter: ${badGuess}`)
-  }
-}
 
 //Reduce attempts when guessing incorrectly
 function reduceAttempts(){
   if (results.innerHTML !== 'Match :)') {
     attempts--
-    console.log(`you have ${attempts} attempts left`)
   }
 }
 
@@ -119,10 +104,10 @@ cards.forEach(card => {
   card.addEventListener('click', () => {
     // If the card has the 'clicked' class, do not allow it to be clicked again
     if (card.classList.contains('clicked')) {
-      return;
+      return
     }
     // Add the 'clicked' class to the card
-    card.classList.add('clicked');
+    card.classList.add('clicked')
     // show the image when the card is clicked
     card.querySelector('img').style.display = 'block'
     
@@ -145,10 +130,9 @@ cards.forEach(card => {
       if (card1 != card2) {
         setTimeout(hideUnmatched, 0700)
       }
-      
       grabResults()
       winGame()
-      loseGame()
+      setTimeout(loseGame, 0700)
  
       // remove 'clicked' class
       cards.forEach(card => {
